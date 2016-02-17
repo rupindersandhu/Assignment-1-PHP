2a61b144d52a5caaf544d288574bd5afa58591a5
mebighobo <john_chang69@hotmail.com>
2016-02-17 11:19:09 -0800

    fix some html

--- application/views/JohnTemplet/templete.php
+++ application/views/JohnTemplet/templete.php
@@ -17,10 +17,47 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/scripts.js"></script>
   </head>
-  
+
+  </head>
   <body>
-      
-      {pagecontent}
-      
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<nav class="navbar navbar-default navbar-inverse" role="navigation">
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+                                           
+						<li class="active">
+							<a href="#">Home</a>
+						</li>
+                                                <li>
+							<a href="#">Player</a>
+						</li>
+						<li>
+							<a href="#">StockInfo</a>
+						</li>
+                                                <li>
+                                                    {login-menu}
+                                                </li>
+					</ul>
+					
+				</div>
+				
+			</nav>
+			
+			<div class = "container-fluid">
+				<img class = "img-rounded center-block" alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/">
+			</div>
+			
+			<div class="row">
+				<div class="col-md-6">
+				{pagebody}
+			</div>
+		</div>
+	</div>
+</div>
+
+    
   </body>
 </html>
